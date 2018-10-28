f3c36d0897c82a9ad8c71ae17d8ae0fe6d4ec0ca
nishad <nishadcb@gmail.com>
2018-10-28 18:44:27 +0400

    sdasds
    fffff

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'github.io',
+	'hostname' => 'localhost',
 	'username' => '',
 	'password' => '',
 	'database' => '',
